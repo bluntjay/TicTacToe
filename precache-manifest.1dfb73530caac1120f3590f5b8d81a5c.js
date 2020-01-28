786ce15da01c3c00f6647bde7233970adf0df57c
Alex Joshua <ialexjoshua@gmail.com>
2020-01-28 21:46:49 +0200

    Updates

---
+++ precache-manifest.1dfb73530caac1120f3590f5b8d81a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10507e1ae4762edc84f06d263eb03fc8",
+    "url": "/bluntjay/TicTacToe.git/index.html"
+  },
+  {
+    "revision": "8348847c65871a147b7f",
+    "url": "/bluntjay/TicTacToe.git/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0defa0c030dcbfe21351",
+    "url": "/bluntjay/TicTacToe.git/static/css/main.61906c56.chunk.css"
+  },
+  {
+    "revision": "8348847c65871a147b7f",
+    "url": "/bluntjay/TicTacToe.git/static/js/2.64f5fedd.chunk.js"
+  },
+  {
+    "revision": "0defa0c030dcbfe21351",
+    "url": "/bluntjay/TicTacToe.git/static/js/main.0fa09e74.chunk.js"
+  },
+  {
+    "revision": "2f2d8c78ec7a4065b262",
+    "url": "/bluntjay/TicTacToe.git/static/js/runtime~main.80e68acc.js"
+  }
+]);
